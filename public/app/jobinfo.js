bb6aec3d11c0337ff1784e61af5e96b8b162aa81
dheisterberg <dheisterberg@osc.edu>
2017-08-22 12:50:05 -0400

    bump versions

--- public/app/jobinfo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var JobInfo = (function () {
-    function JobInfo() {
-    }
-    return JobInfo;
-}());
-exports.JobInfo = JobInfo;
-//# sourceMappingURL=jobinfo.js.map
